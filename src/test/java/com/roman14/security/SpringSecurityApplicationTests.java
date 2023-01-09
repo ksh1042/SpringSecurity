1360e19eee9ae1fd8da6f54fc463466514cd82c7
= <roman14@naver.com>
2023-01-09 23:37:36 +0900

    init

---
+++ src/test/java/com/roman14/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,15 @@
+package com.roman14.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
